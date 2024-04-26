2a35d9d9b23f241f2ba3085b9bb401f0437d1948
Andi <andres.estradag@campusucc.edu.co>
2024-04-25 20:12:20 -0500

    Conexión Base de Datos

--- src/index.js
+++ src/index.js
@@ -47,6 +47,24 @@ root.render(
   </React.StrictMode>
 );
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4b2jqZXQiZNRMMnYWAVqY1mM7C1TH-pA",
+  authDomain: "aquemarropa-4f5d1.firebaseapp.com",
+  projectId: "aquemarropa-4f5d1",
+  storageBucket: "aquemarropa-4f5d1.appspot.com",
+  messagingSenderId: "957110858258",
+  appId: "1:957110858258:web:863170c84670e3e417decb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

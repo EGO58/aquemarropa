9c6cbb451a81f63bea10417403e14cc376121e72
Diego <diego.ramirezr@campusucc.edu.co>
2024-05-22 18:53:37 -0500

    pagina culto

--- src/componentes/Cultos.js
+++ src/componentes/Cultos.js
@@ -3,7 +3,7 @@ import './Estilo-Cultos.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Ojo from '../assets/post-02.jpg';
+import Ojo from '../assets/ojo_V3.gif';
 import Cabezote from './Cabezote';
 import Footer from './Footer';
 
@@ -20,8 +20,10 @@ function Cultos() {
 
             <Row>
                 <Col>
-                    <div className="image-container-galeria">
-                        <img src={Ojo} alt="Gif Ojo" className="gif-ojo" />
+                    <div className="image-container-ojo">
+                        <a href="https://forms.gle/96V9U6ut3Kwc2HW46" target="_blank" rel="noopener noreferrer">
+                            <img src={Ojo} alt="Gif Ojo" className="gif-ojo" />
+                        </a>
                     </div>
                 </Col>
             </Row>
